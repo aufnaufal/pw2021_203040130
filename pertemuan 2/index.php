f18b559a702551a53ac898450affaee2e17c2b8d
aufnaufal <naufalauf14@gmail.com>
2021-02-19 00:09:02 +0700

    menyelesaikan vidio pertemuan ke 2

---
+++ pertemuan 2/index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Auf naufal kosasih
+203040130
+https://github.com/aufnaufal/pw2021_203040130
+pwetemuan 2 - 8 februari 2021
+mempelajari mengenai sintaks php
+*/
+?>
+<?php
+// pengkondisian / percabangan
+// if else
+// if else if else
+// ternary
+// switch
+ $x = 2;
+ if( $x < 20 ) {
+     echo "benar";    
+ } else if( $x == 20 ) {
+     echo "bingo!";    
+ } else {
+    echo "salah";
+ }
+ ?>
